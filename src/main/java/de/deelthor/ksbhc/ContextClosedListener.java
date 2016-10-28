d05b4fa345fe1544fafaaf0c08478f458568fdfa
deelthor <thorsten.deelmann@codecentric.de>
2016-10-28 15:43:14 +0200

    added different way of deleting the ip

--- src/main/java/de/deelthor/ksbhc/ContextClosedListener.java
+++ src/main/java/de/deelthor/ksbhc/ContextClosedListener.java
@@ -1,7 +1,14 @@
 package de.deelthor.ksbhc;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
+import com.hazelcast.map.AbstractEntryProcessor;
+import com.hazelcast.query.EntryObject;
+import com.hazelcast.query.PredicateBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
@@ -16,6 +23,8 @@ import de.deelthor.ksbhc.controller.Lock;
 @Component
 public class ContextClosedListener implements ApplicationListener<ContextClosedEvent> {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private HazelcastInstance hazelcastInstance;
 
@@ -24,14 +33,40 @@ public class ContextClosedListener implements ApplicationListener<ContextClosedE
 
     @Override
     public void onApplicationEvent(ContextClosedEvent event) {
-        IMap<String, Lock> map = hazelcastInstance.getMap(Application.HC_MAP_NAME);
-        Set<String> keys = map.keySet();
-        for (String key : keys) {
-            Lock lock = map.get(key);
-            if (podIP.equals(lock.getIp())) {
-                map.remove(key);
+        IMap<String, Lock> locks = hazelcastInstance.getMap(Application.HC_MAP_NAME);
+        EntryObject entryObj = new PredicateBuilder().getEntryObject();
+        PredicateBuilder predicate = entryObj.get( "ip" ).equal( podIP );
+
+        try {
+            Map<String, Object> deletedEntries = locks.executeOnEntries(new DeletePodEntriesProcessor(podIP), predicate);
+            if(deletedEntries.size() > 0) {
+                logger.info("removed locks for {}: {}", podIP, deletedEntries);
             }
+        } catch (Exception e) {
+            logger.error("Error occured while removing locks for {}: {}", podIP, e);
+        }
+    }
+
+
+    private static class DeletePodEntriesProcessor extends AbstractEntryProcessor<UUID, Lock> {
+        private static final long serialVersionUID = -1365634527454605455L;
+
+        private final String podIP;
+
+        public DeletePodEntriesProcessor(String podIP) {
+            this.podIP = podIP;
         }
 
+        @Override
+        public Object process(Map.Entry<UUID, Lock> entry) {
+            Lock lock = entry.getValue();
+//            logger.info("check PartitionLock {} for {}", lock, podIP);
+            if (podIP.equals(lock.getIp())) {
+//                logger.info("remove PartitionLock {} for {}", lock, podIP);
+                entry.setValue(null);
+                return lock.toString();
+            }
+            return null;
+        }
     }
 }
